824c94ee41c01b55211c1f2749a82b7c2e3f81e4
kijimaru <eddix.ibanez@outlook.jp>
2020-03-07 14:43:51 +0900

    in propress

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MDBCard, MDBCardTitle, MDBContainer } from 'mdbreact';
+
+export default function Home() {
+  return (
+    <>
+      <MDBContainer>
+        <MDBCard>
+          
+        </MDBCard>
+      </MDBContainer>
+    </>
+  );
+}
